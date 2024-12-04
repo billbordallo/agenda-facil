5e11fb03233d58938e05e0a72b581029562baa9c
Fabiano Bordallo <fabiano@treeideiasdigitais.com.br>
2024-12-04 14:16:44 -0300

    Ajustes gerais de layout

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { AppBar, Toolbar, Box } from '@mui/material';
 import logo from '../assets/logo-horizontal.svg';
@@ -12,17 +13,22 @@ const Header = ({ setContentMargin }) => {
       <header className="header">
         <AppBar position="static" sx={{ backgroundColor: '#4C77DB', height: '70px', width: '100%' }}>
           <Toolbar>
-            <Box
-              component="img"
-              src={logo}
-              alt="Logo"
-              sx={{ width: '157px', height: '27.4px', marginRight: 'auto' }}
-            />
+            <Link to="/" style={{ position: 'relative', top: '3px', margin: '0 auto' }} >
+              <Box
+                component="img"
+                src={logo}
+                alt="Logo"
+                sx={{ width: '157px', height: '27.4px', marginRight: 'auto' }}
+              />
+            </Link>
           </Toolbar>
         </AppBar>
       </header>
     </>
   );
+};
+Header.propTypes = {
+  setContentMargin: PropTypes.func.isRequired,
 };
 
 export default Header;
